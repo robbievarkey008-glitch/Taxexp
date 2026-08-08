1b885d9a8746dfced7de6f792461ee965d10143a
Rob <rob@3a:f2:1b:0c:ca:87.home>
2026-08-07 23:09:06 -0400

    Fix critical CORS 404 error during form submission by routing UploadThing through Shopify App Proxy

--- app/routes/api.proxy.tsx
+++ app/routes/api.proxy.tsx
@@ -447,7 +447,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 
   import { genUploader } from "https://esm.sh/uploadthing@7.4.1/client?deps=react";
   const { uploadFiles } = genUploader({
-    url: "${appUrl}/api/proxy/uploadthing",
+    url: window.location.pathname + "/uploadthing",
   });
 
   window.submitForm = async function() {
